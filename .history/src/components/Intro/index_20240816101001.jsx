4338c34897c07cc1108f59980a32f2d22fcc36b8
sem1103 <sem.sem.geo.az1998@mail.ru>
2024-08-16 12:54:44 +0400

    header and intro done

---
+++ .history/src/components/Intro/index_20240816101001.jsx
@@ -0,0 +1,15 @@
+import s from './style.module.css'
+
+export default function Intro() {
+
+
+    return (
+        <div className={s.intro}>
+            <h1>
+            The Value of
+            <br />
+            <span>Human Capital</span>
+            </h1>
+        </div>
+    )
+}
